5362f7469c2a7439ea2e6ecde73f557da06be9ff
patrickbindelli <patrickvenanbindelli@gmail.com>
2023-03-11 15:50:25 -0300

    adding base main page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import MainPage from '@/components/MainPage';
 
 export default function Home() {
   return (
@@ -10,9 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>
-          <h1>Hello, world!</h1>
-        </div>
+        <MainPage />
       </main>
     </>
   );
