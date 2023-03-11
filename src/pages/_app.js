8e5103cc6b7218d7a869237c6ed1f89c029f74bf
patrickbindelli <patrickvenanbindelli@gmail.com>
2023-03-11 15:50:00 -0300

    adding global theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import GlobalStyle from '@/styles/global';
+import theme from '@/styles/themes/dark';
+import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
