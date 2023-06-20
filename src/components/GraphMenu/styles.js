cb1b44291aee985bf4e39ec696d6a065eb19ed0e
patrickbindelli <patrickvenanbindelli@gmail.com>
2023-06-20 19:22:21 -0300

    finishing graph config

--- src/components/GraphMenu/styles.js
+++ src/components/GraphMenu/styles.js
@@ -14,6 +14,12 @@ export const Container = styled.div`
   border-radius: 10px;
   gap: 10px;
   padding: 10px;
+  opacity: 0;
+  transition: opacity 250ms ease-in-out;
+
+  :hover {
+    opacity: 1;
+  }
 `;
 
 export const TopMenu = styled.div`
--- src/components/GraphRender/index.jsx
+++ src/components/GraphRender/index.jsx
@@ -1,14 +1,13 @@
-import CytoscapeComponent from "react-cytoscapejs";
+import CytoscapeComponent from 'react-cytoscapejs';
 import { useTheme } from 'styled-components';
 import cytoscape from 'cytoscape';
-import cise from "cytoscape-cise";
+import cise from 'cytoscape-cise';
 
-const GraphRender = ({graphData, upperLimit, lowerLimit, colors, isLoading}) => {
-
+const GraphRender = ({ graphData, upperLimit, lowerLimit, colors, isLoading }) => {
   const theme = useTheme();
-  cytoscape.use(cise)
+  cytoscape.use(cise);
   const layout = {
-    name: "cise",//"random"
+    name: 'cise', //"random"
     fit: true,
     directed: false,
     padding: 10,
@@ -16,84 +15,81 @@ const GraphRender = ({graphData, upperLimit, lowerLimit, colors, isLoading}) => 
     animationDuration: 1000,
     refresh: 50,
     avoidOverlap: true,
-    nodeDimensionsIncludeLabels: true
+    nodeDimensionsIncludeLabels: true,
   };
 
   const styleSheet = [
     {
-      selector: "node",
+      selector: 'node',
       style: {
         width: 30,
         height: 30,
-        label: "data(label)",
-        "z-index": "10",
-        color: "white",
+        label: 'data(label)',
+        'z-index': '10',
+        color: 'white',
         fontSize: 20,
-        'background-color'(n){
-          if (n.data("count") >= upperLimit){
-            return colors.thirdColor}
-          else if(n.data("count") >= lowerLimit )
-            return colors.secondColor
-          else return colors.firstColor
-        }, 
-       
-      }
+        'background-color'(n) {
+          if (n.data('count') >= upperLimit) {
+            return colors.thirdColor;
+          } else if (n.data('count') >= lowerLimit) return colors.secondColor;
+          else return colors.firstColor;
+        },
+      },
     },
     {
-      selector: "node.hover",
+      selector: 'node.hover',
       style: {
-        "border-width": "3px",
-        "border-color": "#AAD8FF",
-        "border-opacity": "0.3",
-        "background-color": "#77828C",
+        'border-width': '3px',
+        'border-color': '#AAD8FF',
+        'border-opacity': '0.3',
+        'background-color': '#77828C',
         'text-valign': 'bottom',
-        label: "data(count)",
+        label: 'data(count)',
         width: 30,
         height: 30,
-      }
+      },
     },
     {
       selector: "node[type='institute']",
       style: {
-        shape: "rectangle"
-      }
+        shape: 'rectangle',
+      },
     },
     {
-      selector: "edge",
+      selector: 'edge',
       style: {
         width: 3,
-        "line-color": theme.colors.bordersAndFocusRing,
-        "target-arrow-shape": "none",
-      }
-    }
+        'line-color': theme.colors.bordersAndFocusRing,
+        'target-arrow-shape': 'none',
+      },
+    },
   ];
-  
+
   return (
     <>
-      {
-      !isLoading && graphData && 
-      <CytoscapeComponent
-        elements={CytoscapeComponent.normalizeElements(graphData)}
-        wheelSensitivity= {0.7}
-        style={{ width: '100%', height: '100%' }}
-        zoomingEnabled={true}
-        maxZoom={1.5}
-        minZoom={0.3}
-        autounselectify={false}
-        boxSelectionEnabled={true}
-        layout={layout}
-        stylesheet={styleSheet}
-        cy={(cy) => {cy.on('mouseover', 'node', function(e) {
-          e.target.addClass('hover');
-          cy.on('mouseout', 'node', function(e) {
-            e.target.removeClass('hover');
-          });
-        }); }}
-        
-      />
-      }
+      {!isLoading && graphData && (
+        <CytoscapeComponent
+          elements={CytoscapeComponent.normalizeElements(graphData)}
+          wheelSensitivity={0.7}
+          style={{ width: '100%', height: '100%' }}
+          zoomingEnabled={true}
+          maxZoom={1.5}
+          minZoom={0.3}
+          autounselectify={false}
+          boxSelectionEnabled={true}
+          layout={layout}
+          stylesheet={styleSheet}
+          cy={(cy) => {
+            cy.on('mouseover', 'node', function (e) {
+              e.target.addClass('hover');
+              cy.on('mouseout', 'node', function (e) {
+                e.target.removeClass('hover');
+              });
+            });
+          }}
+        />
+      )}
     </>
-
   );
 };
 
